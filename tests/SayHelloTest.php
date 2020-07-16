193bb6d78c3194cb728989cccd4675fce0fe5fbc
oleksandr-karagiaur <oleksandr-karagiaur@users.noreply.github.com>
2020-07-16 16:49:39 +0300

    ch

---
+++ tests/SayHelloTest.php
@@ -0,0 +1,21 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class SayHelloTest extends TestCase
+{
+    /**
+     * @dataProvider positiveDataProvider
+     */
+    public function testPositive($input, $expected)
+    {
+        $this->assertEquals($expected, sayHello($input));
+    }
+
+    public function positiveDataProvider()
+    {
+        return [
+            ['Hello', 'Hello']
+        ];
+    }
+}
